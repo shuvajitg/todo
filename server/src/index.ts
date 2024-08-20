68034a140a41b590e324f33295c23ff6157ee490
shuvajit gayen <shuvajitgayen57@gmail.com>
2024-08-20 17:40:49 +0530

    mejor change in backend

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,21 +8,21 @@ app.use(cors());
 
 
 // Logging Middleware
-app.use((req, _, next) => {
-  console.log(`Received ${req.method} request to ${req.url}`);
-  console.log('Request Body:', req.body); // You might want to use a body parser to see this
-  next();
-});
+// app.use((req, _, next) => {
+//   console.log(`Received ${req.method} request to ${req.url}`);
+//   console.log('Request Body:', req.body); // You might want to use a body parser to see this
+//   next();
+// });
 
 app.use('/trpc',trpcExpress.createExpressMiddleware({
   router:appRouter
 }))
 
 
-const port = 3000
+const PORT = process.env.PORT || 3000
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
 
 export type AppRouter = typeof appRouter
